f812373c3fbcd419fc10c9adbf50a7ad3a8646b9
ThinKPadl13 <andre_mir85@mail.ru>
2024-09-14 23:26:35 +0300

    Initial commit

---
+++ lua/core/mappings.lua
@@ -0,0 +1,35 @@
+-- Leader
+vim.g.mapleader = " "
+
+-- Insert
+vim.keymap.set('i', 'jj', '<Esc>')
+
+
+-- Buffers
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>q', ':q<CR>')
+
+
+-- Navigation
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Splits
+vim.keymap.set('n', '|', ':vsplit<CR>')
+vim.keymap.set('n', '\\', ':split<CR>')
+
+-- Neo-tree
+vim.keymap.set('n', '<leader>e', ':Neotree left reveal toggle<CR>')
+
+--Tabs
+vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
+vim.keymap.set('n', '<c-x>', ':BufferLineCloseOthers<CR>')
+
+-- Terminal
+vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float<CR>')
+vim.keymap.set('n', '<leader>th', ':ToggleTerm direction=horizontal<CR>')
+vim.keymap.set('n', '<leader>tv', ':ToggleTerm direction=vertical size=40<CR>')
