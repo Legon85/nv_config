de31e3001374e374abe262f56137816c1a481c0f
ThinKPadl13 <andre_mir85@mail.ru>
2024-09-28 12:25:29 +0300

    added fugitive dashboard leap which-key

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,38 @@
+return {
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	config = function()
+		require("dashboard").setup({
+			theme = "hyper",
+			config = {
+				week_header = {
+					enable = true,
+				},
+				shortcut = {
+					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+					{
+						icon = " ",
+						icon_hl = "@variable",
+						desc = "Files",
+						group = "Label",
+						action = "Telescope find_files",
+						key = "f",
+					},
+					{
+						desc = " Menu",
+						group = "DiagnosticHint",
+						action = "Neotree left toggle reveal",
+						key = "e",
+					},
+					{
+						desc = " dotfiles",
+						group = "Number",
+						action = "Telescope dotfiles",
+						key = "d",
+					},
+				},
+			},
+		})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+}
