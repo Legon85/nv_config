425f69dbb6a34a674c46ddf798fa6a0ee4311631
Youbuntu <andre_mir85@mail.ru>
2024-10-27 21:45:02 +0300

    added hydra dap mappings dap-virtual-text

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -8,6 +8,7 @@ return {
 		dependencies = {
 			"mfussenegger/nvim-dap",
 			"nvim-neotest/nvim-nio",
+			"theHamsta/nvim-dap-virtual-text",
 		},
 		config = function()
 			local dap = require("dap")
@@ -23,12 +24,12 @@ return {
 			dap.listeners.after.launch.dapui_config = function()
 				dapui.open()
 			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
+			--	dap.listeners.before.event_terminated.dapui_config = function()
+			--		dapui.close()
+			--	end
+			--	dap.listeners.before.event_exited.dapui_config = function()
+			--		dapui.close()
+			--	end
 		end,
 	},
 }
